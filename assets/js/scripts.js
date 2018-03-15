40b2628d943728ad54d520d3e5e809a9b3357b45
unknown <mmartinezt7@gmail.com>
2018-03-15 14:56:28 -0300

    proyect commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+
+
+//receive calls from typescript code to update the layouts
+var AdminLTE = (function() {
+  return {
+    init: function() {
+      $(function(){
+        $.AdminLTE.layout.activate();
+        $.AdminLTE.layout.fix();
+        $.AdminLTE.layout.fixSidebar();
+      });
+    }
+  }
+})(AdminLTE||{});
+
